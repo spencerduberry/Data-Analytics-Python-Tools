df6a6addc457965a8e4a9913742c7dceefe9918a
spencerduberry <spencerduberry@hotmail.co.uk>
2025-01-07 21:41:51 +0000

    Refactor: revise encode_col function
    - Added a docstring to improve function documentation
    - Uninitialised col parameter to avoid confusion and promote extensibility

--- Pre-processing/encode_col.py
+++ Pre-processing/encode_col.py
@@ -1,6 +1,14 @@
 import pandas as pd
 
-# DataFrame and column name taken as input. Dataframe returned.
 def encode_col(df: pd.DataFrame, col:str) -> pd.DataFrame:
-    df[f'{col}_encoded'] = df[col].map({k: i for i, k in enumerate(sorted(df["gender"].unique()))})
+    """Encodes a column of a Dataframe by mapping unique values to integers
+
+    Args:
+        df (pd.DataFrame): The input DataFrame containing the column to be encoded
+        col (str): The name of the column to be encoded
+
+    Returns:
+        pd.DataFrame: A DataFrame containing the encoded column in addition to the original data
+    """
+    df[f'{col}_encoded'] = df[col].map({k: i for i, k in enumerate(sorted(df[col].unique()))})
     return df
